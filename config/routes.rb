e7ed8e9d1d1b857486967abfac105ab96448a83d
lea-lp <lea.leipp@gmail.com>
2018-10-30 11:51:41 +0100

    fix paths w/ link_to 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'gossip#index'
+
+  get '/welcome', to: 'bienvenue#welcome'
   get '/team', to: 'static#team'
   get '/contact', to: 'static#contact'
+  
   get '/:id', to: 'bienvenue#one_gossip'
-  get '/welcome', to: 'bienvenue#welcome'
 end
